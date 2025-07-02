a8bc2304ff8c2aca3942e025f722532fef68e605
Nate Stone <nate.stone@opendatarepository.org>
2025-07-02 03:59:15 +0000

    URL fix.

--- functions.php
+++ functions.php
@@ -100,10 +100,10 @@ add_action( 'pre_handle_404', function() {
                 && !preg_match('/^\/ima-mineral-list\/$/i', $current_uri)
                 && !preg_match('/^\/amcsd$/i', $current_uri)
                 && !preg_match('/^\/amcsd\/$/i', $current_uri)
-                && !preg_match('/^\/contact-us$/i', $current_uri)
-                && !preg_match('/^\/contact-us\/$/i', $current_uri)
-                && !preg_match('/^\/contribute$/i', $current_uri)
-                && !preg_match('/^\/contribute\/$/i', $current_uri)
+                && !preg_match('/^\/about\/contact-us$/i', $current_uri)
+                && !preg_match('/^\/about\/contact-us\/$/i', $current_uri)
+                && !preg_match('/^\/about\/contribute$/i', $current_uri)
+                && !preg_match('/^\/about\/contribute\/$/i', $current_uri)
             ) {
                 // Matches RRUFF IDs
                 // {"dt_id":"738","7069":"r040032"}
