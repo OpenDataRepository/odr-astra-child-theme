559a022e3b5c66e985f14d043d5c01368c897141
Nate Stone <nate.stone@opendatarepository.org>
2025-09-18 00:06:10 +0000

    Completing dependency list.

--- functions.php
+++ functions.php
@@ -776,6 +776,10 @@ function runODRKernel($send = false) {
 }
 
 
+/*
+ * Dependency Installer
+ * https://github.com/afragen/wp-dependency-installer
+ */
 require_once __DIR__ . '/vendor/autoload.php';
 add_action( 'plugins_loaded', static function() {
     WP_Dependency_Installer::instance( __DIR__ )->run();
